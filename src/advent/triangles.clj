523b169ade7013394ae63aea239117ef018cdbdf
Philip Jagielski <philip.jagielski@climate.com>
2016-12-08 20:58:59 -0600

    Day 3 Triangles: refactor before puzzle 2

--- src/advent/triangles.clj
+++ src/advent/triangles.clj
@@ -15,12 +15,16 @@
         other-two (apply + (rest triangle)) ]
     (> other-two longest)))
 
+; triangles specified horizontally, 1 per line
+(def get-triangles-puzzle-1 identity)
+
 (defn print-num-legal-triangles
   []
   (->> (read-triangles)
        (map str/trim)
        (map #(str/split % #"\s+"))
        (map #(map (fn [s] (Integer/parseInt s)) %))
+       get-triangles-puzzle-1
        (filter is-triangle-legal?)
        count
        (str "Number of legal triangles: ")
