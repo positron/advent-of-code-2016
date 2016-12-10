07847391648b11ad95779da1210c2b7acdcf51fd
Philip Jagielski <philip.jagielski@climate.com>
2016-12-09 19:50:03 -0600

    Day 4 Puzzle 2: Encrypted Rooms

--- src/advent/rooms.clj
+++ src/advent/rooms.clj
@@ -12,6 +12,7 @@
   [room]
   (let [room (str/split room #"[-\[\]]+")
         [sector-id checksum] (take-last 2 room)
+        sector-id (Integer/parseInt sector-id)
         room-name (str/join "-" (drop-last 2 room))]
     {:name room-name
      :sector-id sector-id
@@ -50,5 +51,29 @@
 (defn sum-of-valid-sector-ids
   []
   (->> (get-valid-rooms)
-       (map (comp #(Integer/parseInt %) :sector-id))
+       (map :sector-id)
        (reduce +)))
+
+(defn shift-char
+  "Shift char by amount, except - turns into a space"
+  [amount ch]
+  (if (= ch \-)
+    \ 
+    (let [a (int \a)
+          ch (- (int ch) a)
+          shifted (mod (+ ch amount) 26)
+          shifted (+ shifted a)]
+      (char shifted))))
+
+(defn decrypt-room
+  [room]
+  (let [sector-id (:sector-id room)
+        encrypted-name (:name room)
+        decrypted-name (str/join (map (partial shift-char sector-id) encrypted-name))]
+    (assoc room :name decrypted-name)))
+
+(defn search-for-north-pole-room
+  []
+  (->> (get-valid-rooms)
+       (map decrypt-room)
+       (filter #(re-find #"north.*pole" (:name %)))))
